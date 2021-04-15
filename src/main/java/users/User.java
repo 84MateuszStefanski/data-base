60335bfa234c88ebb7ee0ec6fa1f2e3f73c85ef7
84MateuszStefanski <84mateuszstefanski@gmail.com>
2021-04-15 21:01:19 +0200

    Lombok

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -1,7 +1,17 @@
 package users;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+@Getter
+@Setter
 @Entity
 @Table(name = "user")
 public class User {
@@ -23,74 +33,10 @@ public class User {
     @Column(name = "age")
     private String age;
 
-    public User() {
-    }
-
     public User(String name, String surname, String nationality, String age) {
         this.name = name;
         this.surname = surname;
         this.nationality = nationality;
         this.age = age;
     }
-
-    public User(long id, String name, String surname, String nationality, String age) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.nationality = nationality;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", nationality='" + nationality + '\'' +
-                ", age='" + age + '\'' +
-                '}';
-    }
-
-
 }
